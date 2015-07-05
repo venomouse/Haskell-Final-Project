46a7b09483315e46d2b8d4e3fbecc2bcaf06de1d
Shai Wyborski <shai.wyborski@mail.huji.ac.il>
2015-07-05 20:24:57 +0300

    a

--- Image.hs
+++ Image.hs
@@ -1,17 +1,17 @@
 import Matrix
 import Data.List
 
-type Image = Matrix Bool
+type Image = Matrix Int
 
 --instance Show Image where
 --    show img = rowfold (\p acc -> (if p then '*' else '.'):acc) "" img
 
 display :: Image -> String
 display img = b ++ "\n" ++ intercalate "\n" r ++ "\n" ++ b ++ "\n" where 
-    r = rowfold (\p acc -> (if p then '█' else ' '):acc) "" img
-    b = replicate (cols img) '-'
+    r = rowfold (\p acc -> (if (p==255) then "██" else "  ") ++ acc) "" img
+    b = replicate ((cols img)*2) '-'
 
 blank :: Int -> Int -> Image
-blank n m = create (replicate n (replicate m False))
+blank n m = create (replicate n (replicate m 0))
 
-main = putStr $ display (set (blank 10 10) 4 4 True)
+main = putStr $ display (set (blank 100 100) 4 4 255)
--- Matrix.hs
+++ Matrix.hs
@@ -1,6 +1,46 @@
+module Matrix
+(   
+    Matrix
+,   create
+,   fold
+,   rowfold
+,   cols
+,   set
+) where
+
 import Control.Monad
 import Control.Applicative
 
-newtype Pixel = Pixel Float
+data Matrix a = Mat [[a]]
 
-data Image = 
+instance (Show a) => Show (Matrix a) where
+    show (Mat d) = concatMap r d where
+        r x = concatMap ((++ "\t") . show) x ++ "\n"
+
+instance Functor Matrix where
+    fmap f (Mat d) = create $ map (map f) d
+
+fold :: (a -> b -> a) -> a -> Matrix b -> a
+fold f acc (Mat d) = foldl f acc (foldr (++) [] d)
+
+rowfold :: (a -> b -> b) -> b -> Matrix a -> [b]
+rowfold f acc (Mat d) = map (foldr f acc) d
+
+create :: [[a]] -> Matrix a
+create (d:ds) 
+    | balanced (length d) ds    = Mat (d:ds)
+    | otherwise                 = error "Matrix rows are imbalanced!"
+    where balanced n = all $ (n ==) . length
+
+set :: Matrix a -> Int -> Int -> a -> Matrix a
+set (Mat d) n m r = Mat (replace d n (replace (d !! n) m r))
+
+replace :: [a] -> Int -> a -> [a]
+replace (d:ds) 0 new = (new:ds)
+replace (d:ds) n new = d : (replace ds (n-1) new)
+
+cols :: Matrix a -> Int
+cols (Mat (d:ds)) = length d
+
+main = print $ set (create [[True,True],[False,True]]) 0 1 False
+--rowfold (\p acc -> (if p then '*' else '.'):acc) ""  (create [[True,True],[False,True]])
